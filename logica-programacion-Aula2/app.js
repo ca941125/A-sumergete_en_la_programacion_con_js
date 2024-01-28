bfd67c732436b0b7fbe96ce8d5c0f769db4e6a75
Carolina Aguirre <carolinaaguirre12@gmail.com>
2024-01-27 22:40:29 -0300

    funciones limpiarCaja, condicionesIniciales y reiniciarJuego

--- logica-programacion-Aula2/app.js
+++ logica-programacion-Aula2/app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
+let intentos = 0;
 
 console.log(numeroSecreto);
 
@@ -12,23 +12,47 @@ function asignarTextoElemento(elemento, texto){
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value) 
     
-    console.log(intentos);
     if ( numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else {
+        //El usuario no acertó
         if (numeroDeUsuario > numeroSecreto){
             asignarTextoElemento('p','El número es menor');
         } else {
             asignarTextoElemento('p','El número es mayor');
         }
     }
+    limpiarCaja();
     intentos++;
-    return;
+}
+function limpiarCaja() {
+    // let valorCaja = document.querySelector('#valorUsuario');
+    // valorCaja.value = '';  forma reducida abajo
+    document.querySelector('#valorUsuario').value = '';
 }
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 10: ');
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10: ');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+
+condicionesIniciales();
